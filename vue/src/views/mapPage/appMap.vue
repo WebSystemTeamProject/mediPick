724c45d9667d5ff319a6073ad66e5ed7b39ec920
MinGW <kmg8280@gmail.com>
2019-11-27 19:44:56 +0900

    make Search page : load Map

--- vue/src/views/mapPage/appMap.vue
+++ vue/src/views/mapPage/appMap.vue
@@ -9,13 +9,15 @@
 <script>
     export default {
     name: "appMap",
-    mounted() {
-        var container = this.$refs.map;
-        var options = {
-            center: new kakao.maps.LatLng(33.450701, 126.570667),
-            level: 3
-        };
-        var map = new kakao.maps.Map(container, options);
+    method: {
+        loadMap(){
+            var container = this.$refs.map;
+            var options = {
+                center: new kakao.maps.LatLng(33.450701, 126.570667),
+                level: 3
+            };
+            var map = new kakao.maps.Map(container, options);
+        }
     }
   }
 </script>
