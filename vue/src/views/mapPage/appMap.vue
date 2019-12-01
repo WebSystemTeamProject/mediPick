0d4e7c0ef8a700de05203a2c0098967d099429f8
MinGW <kmg8280@gmail.com>
2019-12-01 21:04:49 +0900

    지도 초기좌표 아주대로 설정

--- vue/src/views/mapPage/appMap.vue
+++ vue/src/views/mapPage/appMap.vue
@@ -20,10 +20,10 @@ loadScriptOnce('//dapi.kakao.com/v2/maps/sdk.js?appkey=1b1adbf36d0ae9f7eeb8af681
             loadMap(){
                 var container = this.$refs.map;
                 var options = {
-                    center: new kakao.maps.LatLng(33.450701, 126.570667),
+                    center: new kakao.maps.LatLng(37.282895, 127.044938),
                     level: 3
                 };
-  
+                
                 var map = new kakao.maps.Map(container, options);
             }
         }
