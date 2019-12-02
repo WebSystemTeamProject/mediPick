420041cf279e9f835ebf1bce86fb5204c597cb90
MinGW <kmg8280@gmail.com>
2019-12-03 03:27:38 +0900

    API 연동 테스트 코드 추가 및 gitignore 수정

--- node/mAPI.js
+++ node/mAPI.js
@@ -1,9 +1,11 @@
 var request = require('request');
 
-var url = 'http://apis.data.go.kr/B551182/dgamtCrtrInfoService/getCmdcDgamtList';
+var url = 'http://apis.data.go.kr/1471000/MdcBioEqInfoService/getMdcBioEqList';
 var queryParams = '?' + encodeURIComponent('ServiceKey') + '=JyDDOwuO1C7EJrEpMq7YLKHO%2B4wvoZwOUyZKl6DGji8ABGFMQYC2f8QmcSyrw3eG1n5Mou2NkFBe4gsbjB0MuA%3D%3D'; /* Service Key*/
-queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('100'); /* 파라미터설명 */
-queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1');
+queryParams += '&' + encodeURIComponent('item_name') + '=' + encodeURIComponent(''); /* 제품명 */
+queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /* 페이지번호 */
+queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('3'); /* 한 페이지 결과 수 */
+queryParams += '&' + encodeURIComponent('') + '=' + encodeURIComponent(''); /* */
 
 request({
     url: url + queryParams,
