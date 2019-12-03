c67d7df746393d854db08ec75b713ca007d1af06
MinGW <kmg8280@gmail.com>
2019-12-04 03:31:22 +0900

    웹크롤링 코드 작성

--- node/makeDB/webCrawling.js
+++ node/makeDB/webCrawling.js
@@ -10,8 +10,8 @@ var mediLink;
 fs.readFile(fileName,'utf8',async function(err,data){
     if(err) throw err;
     mediLink = await data.split('\r\n');
-    for(let k = 0; k<5; k++){
-        await client.fetch(site + mediLink[k],param,function(err, $, res){
+    for(let k = 0; k<1; k++){
+        await client.fetch(site + mediLink[k],param, function(err, $, res){
             if(err){
                 console.log(err);
                 return;
@@ -19,20 +19,29 @@ fs.readFile(fileName,'utf8',async function(err,data){
             var arr;
             var nameArr;
             var medicineName;
-            var re = /(\s|\||:)+/;
             var newArr = [];
-            $(".inner").each(async function(post){
-                nameArr = await $(this).find(".title").find("strong").text();
-                medicineName=nameArr.substring(0,nameArr.length/2);
+            var re = /(\s|\||:)+/;
+
+            new Promise((resolve, reject) => {
+                $(".inner").each(function(post){
+                    nameArr = $(this).find(".title").find("strong").text();
+                    medicineName= nameArr.substring(0,nameArr.length/2);
+                    resolve(new Promise((resolve, reject) => {
+                      $(".drug_content").each(function(post){
+                            arr = $(this).text().split(re);
+                            for(let i = 0; i<arr.length; i++){
+                                if(arr[i]===' '|| arr[i]==='' || arr[i] === '\n' || arr[i] === '\xA0') continue;
+                                newArr.push(arr[i]);
+                            }
+                            resolve();
+                        })
+                    }));
+                })
+            }).then(() => {
+                console.log(medicineName);
+                console.log(newArr);
             })
-            $(".drug_content").each(async function(post){
-                arr = await $(this).text().split(re);
-                for(let i = 0; i<arr.length; i++){
-                    if(arr[i]===' '|| arr[i]==='' || arr[i] === '\n' || arr[i] === '\xA0') continue;
-                    newArr.push(arr[i]);
-                }
-            })
-            
+
         });
     }
 });
