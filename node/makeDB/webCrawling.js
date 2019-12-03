239709af60e0b2a54432ce4a96a19bc7fc2eee7c
MinGW <kmg8280@gmail.com>
2019-12-04 01:07:36 +0900

    이름 추출 코드 작성

--- node/makeDB/webCrawling.js
+++ node/makeDB/webCrawling.js
@@ -7,21 +7,35 @@ var fs = require('fs');
 var fileName = "dataset.txt";
 var mediLink;
 
-fs.readFile(fileName,'utf8',function(err,data){
+fs.readFile(fileName,'utf8',async function(err,data){
     if(err) throw err;
-    mediLink = data.split('\r\n');
-});
-
-console.log(mediLink);
-/*
-client.fetch(site + excel.link[0],param,function(err, $, res){
-    if(err){
-        console.log(err);
-        return;
+    mediLink = await data.split('\r\n');
+    for(let k = 0; k<5; k++){
+        await client.fetch(site + mediLink[k],param,function(err, $, res){
+            if(err){
+                console.log(err);
+                return;
+            }
+            var arr;
+            var nameArr;
+            var medicineName;
+            var re = /(\s|\||:)+/;
+            var newArr = [];
+            $(".inner").each(async function(post){
+                nameArr = await $(this).find(".title").find("strong").text();
+                medicineName=nameArr.substring(0,nameArr.length/2);
+            })
+            $(".drug_content").each(async function(post){
+                arr = await $(this).text().split(re);
+                for(let i = 0; i<arr.length; i++){
+                    if(arr[i]===' '|| arr[i]==='' || arr[i] === '\n' || arr[i] === '\xA0') continue;
+                    newArr.push(arr[i]);
+                }
+            })
+            
+        });
     }
-
-    $(".r_sec").each(function(post){
-        console.log($(this).text());
-    })
-});*/
+});
+/*
+;*/
 // excel.link.length
