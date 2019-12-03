a5f962ab136e2abfb70196be1b18a88006c11af7
JunseoKim <yuiop8747@ajou.ac.kr>
2019-12-03 22:28:20 +0900

    login,join,register,db link update

--- login&session/login&register&session/backend/routes/login/index.js
+++ login&session/login&register&session/backend/routes/login/index.js
@@ -1,30 +1,20 @@
 var express = require('express');
-var app = express();
 var router = express.Router();
 var path = require('path');
-var mysql = require('mysql');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-
-//Database setting
-var connection = mysql.createConnection({
-    host : 'localhost',
-    port : 3306,
-    user : 'root',
-    password : 'password',
-    database : 'jsman'
-})
-
-connection.connect();
+const User = require("../user/index");
+/* GET home page. */
 
 //router
+
 router.get('/',function(req,res){
     var msg;
     var errMsg = req.flash('error');
     if(errMsg) msg = errMsg;
     console.log('get login url');
-    //res.json({'message' : msg});
-    res.render('login.ejs',{'message' : msg});
+    res.json({'message' : msg});
+    //res.render('login.ejs',{'message' : msg});
 })
 
 passport.serializeUser(function(user,done){
@@ -37,22 +27,23 @@ passport.deserializeUser(function(email,done){
     done(null,email);
 })
 
+
 passport.use('local-login',new LocalStrategy({ //local-join 이라는 이름의 Strategy 사용
     usernameField: 'email',
     passwordField: 'password',
-    pssReqToCallback: true
+    pssReqToCallback: false
 }, function(email,password,done) {
     // DB에 있는 email search
     console.log('email : '+email);
-    var query = connection.query('select * from user where email=? AND pw=?',[email,password],function(err,rows){
-        if(err)return done(err);
-        console.log(rows)
-        if(rows.length){
-            return done(null,{'email' : email,'name' : rows[0].name});
+    User.findOne({email : email},function(err,user){
+        if(err)
+            return done(err);
+        if(!user)
+            return done(null,false,{message : "존재하지 않는 아이디입니다."})
+        if(user.password !== password){
+            return done(null,false,{message : "비밀번호를 잘못 입력하였습니다."});
         }
-        else{
-            return done(null,false,{'message' : "your login info is wrong"})
-        }
+        return done(null,user);
     })
     console.log('local-login callback called');
 }));
@@ -61,16 +52,19 @@ passport.use('local-login',new LocalStrategy({ //local-join 이라는 이름의 
 
 router.post('/',function(req,res,next){
     console.log("login here");
-    passport.authenticate('local-login',function(err,user,info){
-        if(err)return res.status(500).json(err);
-        if(!user) {
-            console.log("user : "+user);
+    passport.authenticate('local-login',function(err,user_,info){
+        if(err) {
+            console.log("err : " + err);
+            return res.status(500).json(err);
+        }
+        if(!user_) {
+            console.log("user : "+user_);
             console.log("message : "+info.message);
             return res.json({trig : false, message : info.message});
         }
-        req.logIn(user,function(err){
+        req.logIn(user_,function(err){
             if(err) return next(err);
-            return res.json({trig : true, user : user});
+            return res.json({trig : true, user : user_});
         });
     })(req,res,next);
 })
