592cf9001baac480f9f0e257f41812de75f3a47d
Yongjae Park <dydwo0710@ajou.ac.kr>
2019-12-07 13:43:32 +0900

    footer 수정

--- vue/src/views/mainPage/appFooter.vue
+++ vue/src/views/mainPage/appFooter.vue
@@ -1,23 +1,24 @@
 <template>
-    <footer>
-        <section>
-            <p>
-                (주) 메디픽즈 (대표 : OOO)<span class="line">|</span>
-                사업자등록번호 : 000-00-00000<span class="line">|</span>
-                통신판매업신고번호 : 제2019-경기수원-00000호
-            </p>
-            <p>
-                소재지 : 경기도 수원시 영통구 월드컵로 206 KR<span class="line">|</span>
-                대표전화 : 000-000-0000<span class="line">|</span>
-                이메일 : qwer@medipicks.com
-            </p>
+    <div class="footerWrap">
+        <footer>
+            <div>
+                <span>(주) 메디픽즈 (대표 : OOO)</span>
+                <span>사업자등록번호 : 000-00-00000</span>
+                <span>통신판매업신고번호 : 제2019-경기수원-00000호</span>
+            </div>
+            <div>
+                <span>소재지 : 경기도 수원시 영통구 월드컵로 206 KR</span>
+                <span>대표전화 : 000-000-0000</span>
+                <span>이메일 : qwer@medipicks.com</span>
+            </div>
             <br/>
-            <p>
+            <br/>
+            <div>
                 Copyright ⓒ MEDIPICKS Inc. All Rights Reserved.
-            </p>
+            </div>
             <img src="../../assets/logo.png">
-        </section>
-    </footer>
+        </footer>
+    </div>
 </template>
 
 <script>
@@ -27,22 +28,89 @@
 </script>
 
 <style scoped>
-    footer {
-        height: 300px;
+    .footerWrap {
+        padding: 40px 16px 30px 16px;
         background-color: #2D9CAE;
+        height: 300px;
+        font-size: 12px;
+        font-weight: 400;
         color: white;
+        box-sizing: border-box;
     }
-    section {
-        padding: 50px 0px 16px 200px;
-        transition: .5s;
+    footer {
+        position: relative;
+        height: 100%;
     }
-    .line {
-        padding-left: 10px;
-        padding-right: 10px;
-        opacity: 0.5;
+    div:first-of-type {
+        margin-bottom: 10px;
     }
+    span {
+        display: block;
+        line-height: 1.5;
+    }
+    /*span::after {*/
+        /*content: "|";*/
+        /*display: inline;*/
+    /*}*/
     img {
-        padding-left: 1000px;
-        padding-top: 70px;
+        width: 100px;
+        position: absolute;
+        bottom: 0;
+        right: 0;
     }
+
+    @media(min-width: 600px) {
+        .footerWrap {
+            padding: 40px 24px 40px 24px;
+            font-size: 14px;
+        }
+        div:first-of-type {
+            margin-bottom: 6px;
+        }
+        span {
+            display: inline-block;
+        }
+        span::after {
+            content: "|";
+            display: inline;
+            margin: 0 6px;
+        }
+        img {
+            width: 120px;
+        }
+    }
+
+    @media(min-width: 1025px) {
+        .footerWrap {
+            padding: 40px 0;
+            font-size: 16px;
+        }
+        footer {
+            width: 1200px;
+            padding: 0;
+            margin: 0 auto;
+        }
+        div:first-of-type {
+            margin-bottom: 10px;
+        }
+        img {
+            width: 160px;
+        }
+    }
+    /*footer {*/
+        /*padding: 50px 0 16px 200px;*/
+        /*height: 300px;*/
+        /*background-color: #2D9CAE;*/
+        /*color: white;*/
+        /*box-sizing: border-box;*/
+    /*}*/
+    /*.line {*/
+        /*padding-left: 10px;*/
+        /*padding-right: 10px;*/
+        /*opacity: 0.5;*/
+    /*}*/
+    /*img {*/
+        /*padding-left: 1000px;*/
+        /*padding-top: 70px;*/
+    /*}*/
 </style>
